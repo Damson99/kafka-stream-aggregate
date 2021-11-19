28d3898309e3dd4303a518d18cd7c968a64af452
dwieczorek <damian.wieczorek@spicasolutions.pl>
2021-11-19 14:29:13 +0100

    Closes #5. adding UserClick time extractor

---
+++ src/main/java/com/cloud/kafkastream/config/UserClickTimeExtractor.java
@@ -0,0 +1,27 @@
+package com.cloud.kafkastream.config;
+
+import com.cloud.kafkastream.web.v1.model.UserClick;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.processor.TimestampExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class UserClickTimeExtractor implements TimestampExtractor {
+
+
+    @Override
+    public long extract(ConsumerRecord<Object, Object> consumerRecord, long prevTime) {
+
+        UserClick userClick= (UserClick) consumerRecord.value();
+        log.info("Click time: {}", userClick.getCreatedTime());
+        return ((userClick.getCreatedTime()>0) ? userClick.getCreatedTime() : prevTime);
+    }
+
+    @Bean
+    public TimestampExtractor userClickTimeExtractor() {
+        return new UserClickTimeExtractor();
+    }
+}
